1a62ba1bafa69345bb95fd7b7ac22a6ea31bb252
Themba Ngobeni <themba.ngobeni@gmail.com>
2021-04-28 02:07:17 +0200

    main

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component } from '@angular/core';
+
+import { AccountService, StudentService, ClassService, GradeService, RosterService, PeriodService, summaryreport_vService,
+        detailreport_vService,teacherService } from './_services';
+import { Csgi_class, Csgi_detailreport_v, Csgi_grade, Csgi_period, Csgi_roster, Csgi_student, Csgi_summaryreport_v, Csgi_teacher_v, User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    user: User;
+    student:Csgi_student;
+    classes:Csgi_class;
+    grade:Csgi_grade;
+    roster:Csgi_roster;
+    period:Csgi_period;
+    detailreport:Csgi_detailreport_v;
+    summaryreport:Csgi_summaryreport_v;
+    teacher:Csgi_teacher_v;
+
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this.user = x);
+    }
+/*
+    constructor(private studentService: StudentService) {
+        this.studentService.student.subscribe(x => this.student = x);
+    }
+
+    constructor(private classService: ClassService) {
+        this.classService.classes.subscribe(x => this.classes = x);
+    }
+*/
+
+    logout() {
+        this.accountService.logout();
+    }
+}
---
+++ src/app/students/list.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { StudentService } from '@app/_services';
+
+@Component({ templateUrl: 'list.component.html' })
+export class ListComponent implements OnInit {
+    students = null;
+
+    constructor(private studentService: StudentService) {}
+
+    ngOnInit() {
+        this.studentService.getAll()
+            .pipe(first())
+            .subscribe(students => this.students = students);
+    }
+
+    deleteUser(id: string) {
+        const user = this.students.find(x => x.id === id);
+        user.isDeleting = true;
+        this.studentService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.students = this.students.filter(x => x.id !== id));
+    }
+}
